80944e076c3aeb95c3c0edad4434150554c5f8da
philong1995 <long.01.04.1995@gmail.com>
2021-10-22 12:48:07 +0700

    update

--- flutter_exercise_2/lib/main.dart
+++ flutter_exercise_2/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_exercise_2/ui/grid.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+// import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,50 +29,50 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
+      backgroundColor: Colors.white,
 
-        /**
-        * phần cuối
-        */
+      // /**
+      // * phần cuối
+      // */
 
-        body: StaggeredGridView.countBuilder(
-          crossAxisCount: 2,
-          mainAxisSpacing: 8,
-          crossAxisSpacing: 8,
-          itemBuilder: (context, index) => const GridUI(),
-          staggeredTileBuilder: (index) => StaggeredTile.count(
-              // index đầu tiên là xét độ rồng của 1 hàng, câu điều kiện : "index chia 5 có phần dư = 0, nếu đúng thì độ rộng của view là 2 (vì 1 hàng tối đa là 2 ô view, nên khi view = 2 thì full(width = 1), nếu sai thì độ rộng của view = 1, bắt đầu từ ô thứ 0 ) "
-              (index % 5 == 0) ? 2 : 1,
-              // index thứ 2 là xét độ dài của 1 cột, câu điều kiện : "index chia 5 có phần dư = 0, nếu đúng thì độ dài của view là 3 (vì 1 ô có width = height, nếu sai thì độ rộng của view = 1) ) "
-              (index % 5 == 0) ? 3 : 1),
-        )
+      // body: StaggeredGridView.countBuilder(
+      //   crossAxisCount: 2,
+      //   mainAxisSpacing: 8,
+      //   crossAxisSpacing: 8,
+      //   itemBuilder: (context, index) => const GridUI(),
+      //   staggeredTileBuilder: (index) => StaggeredTile.count(
+      //       // index đầu tiên là xét độ rồng của 1 hàng, câu điều kiện : "index chia 5 có phần dư = 0, nếu đúng thì độ rộng của view là 2 (vì 1 hàng tối đa là 2 ô view, nên khi view = 2 thì full(width = 1), nếu sai thì độ rộng của view = 1, bắt đầu từ ô thứ 0 ) "
+      //       (index % 5 == 0) ? 2 : 1,
+      //       // index thứ 2 là xét độ dài của 1 cột, câu điều kiện : "index chia 5 có phần dư = 0, nếu đúng thì độ dài của view là 3 (vì 1 ô có width = height, nếu sai thì độ rộng của view = 1) ) "
+      //       (index % 5 == 0) ? 3 : 1),
+      // )
 
-        /**
+      /**
        * Phần lưới
        */
 
-        // body: GridView(
-        //   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        //       // số lượng ô trên 1 hàng
-        //       crossAxisCount: 4,
-        //       //khoảng cách giữa 2 ô trên 1 hàng
-        //       crossAxisSpacing: 8,
-        //       //khoảng cách giữa 2 ô trên 1 cột
-        //       mainAxisSpacing: 8),
-        //   children: [for (int i = 0; i < 35; i++) const GridUI()],
-        // ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            // số lượng ô trên 1 hàng
+            crossAxisCount: 4,
+            //khoảng cách giữa 2 ô trên 1 hàng
+            crossAxisSpacing: 8,
+            //khoảng cách giữa 2 ô trên 1 cột
+            mainAxisSpacing: 8),
+        children: [for (int i = 0; i < 35; i++) const GridUI()],
+      ),
 
-        /**
+      /**
        * Phần hàng và cột
        */
 
-        // body: ListView.builder(
-        //     // với row là Axis.horizontal, còn column là Axis.vertical
-        //     scrollDirection: Axis.vertical,
-        //     itemCount: 7,
-        //     itemBuilder: (context, index) {
-        //       return const ColumnUI();
-        //     })
-        );
+      // body: ListView.builder(
+      //     // với row là Axis.horizontal, còn column là Axis.vertical
+      //     scrollDirection: Axis.vertical,
+      //     itemCount: 7,
+      //     itemBuilder: (context, index) {
+      //       return const ColumnUI();
+      //     })
+    );
   }
 }
