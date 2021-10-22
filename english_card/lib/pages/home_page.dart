0d6544bf5cdcb7a877c875e8ff0ea770e130855b
philong1995 <long.01.04.1995@gmail.com>
2021-10-22 13:17:25 +0700

    update

--- english_card/lib/pages/home_page.dart
+++ english_card/lib/pages/home_page.dart
@@ -27,7 +27,7 @@ class _HomePageState extends State<HomePage> {
           child: Image.asset(AppPath.menuPath),
         ),
       ),
-      body: Container(
+      body: SizedBox(
         width: double.infinity,
         child: Column(
           children: [
